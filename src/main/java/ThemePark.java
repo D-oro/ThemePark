6050c6beedd1bcc2ef3c776557c18fc646d047ab
D-oro <107217252+D-oro@users.noreply.github.com>
2022-09-26 20:39:00 +0100

    started on extensions

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,14 @@
+import attractions.Attraction;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
+//ThemePark has a method getAllReviewed() which returns an ArrayList of IReviewed objects.
+
 public class ThemePark {
+
+    private ArrayList<Attraction> listOfAttractions;
+    private ArrayList<Stall> listOfStalls;
+//    public void addToList(){
+//    }
 }
--- src/main/java/people/Visitor.java
+++ src/main/java/people/Visitor.java
@@ -1,15 +1,31 @@
 package people;
 
+import attractions.Attraction;
+
+import java.util.ArrayList;
+
 public class Visitor {
 
     private int age;
     private double height;
     private double money;
 
+    private ArrayList<Attraction> visitedAttractions;
+//Add an empty visitedAttractions ArrayList to Visitor which stores a
+// list of Attractions and a method to add an attraction to the list.
     public Visitor(int age, double height, double money) {
+        this.visitedAttractions = new ArrayList<Attraction>();
         this.age = age;
         this.height = height;
         this.money = money;
+    }
+
+    public void addAttractionToVisitedAttractions(Attraction attraction){
+        visitedAttractions.add(attraction);
+    }
+
+    public ArrayList<Attraction> getVisitedAttractions() {
+        return visitedAttractions;
     }
 
     public int getAge() {
